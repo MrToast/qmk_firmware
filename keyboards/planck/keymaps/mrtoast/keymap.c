78632ea6a38df5af986cda0ebca60b89db315703
MrToast <4jsandy@gmail.com>
2019-12-01 20:48:31 -0800

    switching ENTER and DELETE to avoid accidental ENTER instead of BACKSPACE

--- keyboards/planck/keymaps/mrtoast/keymap.c
+++ keyboards/planck/keymaps/mrtoast/keymap.c
@@ -71,14 +71,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |   -  |   {  |      |      |   }  |   _  |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | CAPS |      |      |      |      |    DELETE   |      | HOME | PG UP| PG DN| END  |
+ * | CAPS |      |      |      |      |    ENTER    |      | HOME | PG UP| PG DN| END  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV , KC_PIPE, KC_NO  , KC_TILD, KC_LPRN, KC_NO  , KC_NO  , KC_RPRN, KC_PIPE, KC_NO  , KC_MINS, KC_DEL ,
     KC_TRNS, KC_BSLS, KC_NO  , KC_GRV , KC_LBRC, KC_NO  , KC_NO  , KC_RBRC, KC_BSLS, KC_NO  , KC_QUOT, KC_QUOT,
     KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_LCBR, KC_NO  , KC_NO  , KC_RCBR, KC_NO  , KC_NO  , KC_NO  , KC_TRNS,
-    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_DEL , KC_DEL , KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END
+    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_ENT , KC_DEL , KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END
 ),
 
 /* Adjust (Lower + Raise)
@@ -107,14 +107,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |   1  |   2  |   3  | ENTER|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |    ENTER    |      |   0  |   ,  |   .  | BKSP |
+ * |      |      |      |      |      |    DELETE   |      |   0  |   ,  |   .  | BKSP |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] = LAYOUT_planck_grid(
     KC_TRNS, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_NO  , KC_NO  , KC_NO  , KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
     KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_P4  , KC_P5  , KC_P6  , KC_PEQL,
     KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_P1  , KC_P2  , KC_P3  , KC_PENT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_ENT , KC_ENT , KC_NO  , KC_P0  , KC_PCMM, KC_PDOT, KC_BSPC
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_DEL , KC_ENT , KC_NO  , KC_P0  , KC_PCMM, KC_PDOT, KC_BSPC
 )
 
 };
